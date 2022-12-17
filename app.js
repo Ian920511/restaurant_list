9d35bf57de54f05125dcb80b419584d0fd2a943e
Ian_Lin <ian920511@gmail.com>
2022-12-17 22:45:10 +0800

    feat: add deatil page

--- app.js
+++ app.js
@@ -49,11 +49,13 @@ app.post("/restaurants", (req, res) => {
     .catch((error) => console.log(error));
 });
 
-app.get("/restaurant/:id", (req, res) => {
-  const restaurant = restaurantList.results.find(
-    (restaurant) => restaurant.id.toString() === req.params.id
-  );
-  res.render("show", { restaurant });
+app.get("/restaurants/:id", (req, res) => {
+  const id = req.params.id;
+
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render("show", { restaurant }))
+    .catch((error) => console.log(error));
 });
 
 app.get("/search", (req, res) => {
