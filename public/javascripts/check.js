bdec95d8624fe6df9c5236f66306999886b0dbd6
Ian_Lin <ian920511@gmail.com>
2022-12-20 10:55:28 +0800

    refactor: confirm delete

--- public/javascripts/check.js
+++
@@ -1,7 +0,0 @@
-const deleteCheck = document.querySelector(".to-delete");
-
-deleteCheck.addEventListener("click", (event) => {
-  if (event.target.classList.contains("btn-danger")) {
-    alert("Are you sure to delete the restaurant?");
-  }
-});
