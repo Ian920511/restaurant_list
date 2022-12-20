619faec470e9f30108659da9e390d89b8a133603
Ian_Lin <ian920511@gmail.com>
2022-12-20 18:49:39 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 const port = 3000;
 
@@ -31,6 +32,7 @@ app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   Restaurant.find()
@@ -87,7 +89,7 @@ app.get("/restaurants/:id/edit", (req, res) => {
     .catch((error) => console.log(error));
 });
 
-app.post("/restaurants/:id/edit", (req, res) => {
+app.put("/restaurants/:id", (req, res) => {
   const id = req.params.id;
 
   return Restaurant.findById(id)
@@ -96,7 +98,7 @@ app.post("/restaurants/:id/edit", (req, res) => {
     .catch((error) => console.log(error));
 });
 
-app.post("/restaurants/:id/delete", (req, res) => {
+app.delete("/restaurants/:id", (req, res) => {
   const id = req.params.id;
   return Restaurant.findById(id)
     .then((restaurant) => restaurant.remove())
