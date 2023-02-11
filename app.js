cdc09e558af0accc8d747a84173f9f75e4e66156
Ian_Lin <ian920511@gmail.com>
2023-02-11 14:43:06 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const session = require("express-session");
+const flash = require("connect-flash");
 
 const port = 3000;
 
@@ -41,11 +42,14 @@ app.use(methodOverride("_method"));
 
 usePassport(app);
 
+app.use(flash());
 app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.isAuthenticated()
-  res.locals.user = req.user
-  next()
-})
+  res.locals.isAuthenticated = req.isAuthenticated();
+  res.locals.user = req.user;
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.warning_msg = req.flash("warning_msg");
+  next();
+});
 
 app.use(routes);
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -22,32 +22,51 @@ router.get("/register", (req, res) => {
 
 router.post("/register", (req, res) => {
   const { name, email, password, confirmPassword } = req.body;
+  const errors = [];
 
-  User.findOne({ email })
-    .then((user) => {
-      if (user) {
-        console.log("User already Exists");
-        res.render("register", {
-          name,
-          email,
-          password,
-          confirmPassword,
-        });
-      } else {
-        return User.create({
-          name,
-          email,
-          password,
-        })
-          .then(() => res.redirect("/users/login"))
-          .catch((error) => console.log(error));
-      }
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: "所有欄位皆是必填!" });
+  }
+
+  if (password !== confirmPassword) {
+    errors.push({ message: "密碼與確認密碼不相符!" });
+  }
+
+  if (errors.length) {
+    return res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword,
+    });
+  }
+
+  User.findOne({ email }).then((user) => {
+    if (user) {
+      errors.push({ message: "此 Email已經註冊過了!" });
+      return res.render("register", {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword,
+      });
+    }
+    return User.create({
+      name,
+      email,
+      password,
     })
-    .catch((error) => console.log(error));
+      .then(() => req.flash("success_msg", "成功建立帳號請登入!"))
+      .then(() => res.redirect("/users/login"))
+      .catch((error) => console.log(error));
+  });
 });
 
 router.get("/logout", (req, res) => {
   req.logout();
+  req.flash("success_msg", "成功登出!");
   res.redirect("/users/login");
 });
 
