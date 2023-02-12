54953631243c436e197ae4ec1758cd06a94aaad3
Ian_Lin <ian920511@gmail.com>
2023-02-12 15:33:21 +0800

    feat:modify seeder

--- models/seeder/restaurantSeeder.js
+++ models/seeder/restaurantSeeder.js
@@ -1,10 +1,45 @@
+const bcrypt = require("bcryptjs");
+
 const Restaurant = require("../restaurant");
-const restaurantData = require("../../restaurant.json").results;
+const User = require("../user");
+const restaurantData = require("./restaurant.json").results;
+const seed_user = require("./user.json").results;
 const db = require("../../config/mongoose");
 
 db.once("open", () => {
-  Restaurant.create(restaurantData);
+  return Promise.all(
+    seed_user.map((user) => {
+      const { name, email, password, restaurantIndex } = user;
 
-  console.log("its done!");
+      return bcrypt
+        .genSalt(10)
+        .then((salt) => bcrypt.hash(password, salt))
+        .then((hash) =>
+          User.create({
+            name,
+            email,
+            password: hash,
+          })
+        )
+
+        .then((user) => {
+          const filteredRestaurants = restaurantData.filter((index) =>
+            restaurantIndex.includes(index.id)
+          );
+          const userId = user._id;
+
+          return Promise.all(
+            Array.from({ length: filteredRestaurants.length }, (_, i) =>
+              Restaurant.create({ ...filteredRestaurants[i], userId })
+            )
+          );
+        })
+
+        .then(() => {
+          console.log("done!");
+          process.exit();
+        })
+        .catch((error) => console.log(error));
+    })
+  );
 });
-  
